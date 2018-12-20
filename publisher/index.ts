9e8ab500acd2f1bc09ed86357e533b0059d95e05
wslyvh <wslyvh@users.noreply.github.com>
2018-12-20 14:42:14 +0100

    npm publisher

--- publisher/index.ts
+++ publisher/index.ts
@@ -1,5 +1,9 @@
+#!/usr/bin/env node
+
 import { FileManager } from "./src/FileManager";
 import { Publisher } from "./src/Publisher";
 
 const publisher = new Publisher(new FileManager());
 publisher.Publish();
+
+console.log("All done..");
--- publisher/src/config/AppConfig.ts
+++ publisher/src/config/AppConfig.ts
@@ -5,7 +5,7 @@ dotenv.config();
 const AppConfig = {
     NODE_ENV: process.env.NODE_ENV,
 
-    BACKEND_URI: process.env.BACKEND_URI || "http://localhost:5000/"
+    BACKEND_URI: process.env.BACKEND_URI || "http://localhost:5000/",
 };
 
 export default AppConfig;
